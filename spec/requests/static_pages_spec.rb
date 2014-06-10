fe24f9b03503cc5ec8776dd7648b286fcb966e1e
Daniel Hudy <djh3315@gmail.com>
2014-06-10 08:55:43 -0400

    Changing layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,9 +6,14 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -17,7 +22,7 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-    it "should have the title 'Help'" do
+    it "should have the base and custom title" do
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
@@ -28,7 +33,7 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
-    it "should have the title 'About'" do
+    it "should have the base and custom title " do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end
